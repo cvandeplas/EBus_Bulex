51156b6a65d677fac2d38b472fd07566fbba5cc5
Christophe Vandeplas <christophe@vandeplas.com>
2015-02-15 18:58:46 +0100

    additional progress

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ EBus Bules
 =============
 Current research on the EBus protocol implementation by Bulex.
 
+Af first sight it seems that the Bulex implementation ressembles a lot the one from Vaillant.
+
 Great documentation here, but not all applicable for Bulex: 
 
 * http://ebus.webhop.org
--- ebus.py
+++ ebus.py
@@ -36,7 +36,7 @@ fgrep -E -v '\taa' data/test.txt  | awk -F'\t' '{ print $2 }'  | awk '{ print $3
 
 receiver calculates CRC, if OK, ack with 00
 CRC = X^8 + x^7 + x^4 + x^3 + x + 1  ???
-CRC = 0b10011011 = 0x9B
+CRC = 0b110011011
 
 
 ???
@@ -240,8 +240,8 @@ class EBus(Packet):
     name = "eBUS"
 
     ebus_primary_commands = {
-                            0x07: "07 - System Data Commands",
-                            0xb5: "B5 - Bulex Commands"
+                            0x07: "0x07 - System Data Commands",
+                            0xb5: "0xB5 - Bulex Commands"
                              }
     ebus_secundary_commands = {
                  0x07: {0x00: "Date/Time Message of an eBUS Master",
@@ -262,8 +262,9 @@ class EBus(Packet):
                         0x10: "Operational Data from Room Controller to Burner Control Unit",
                         0x11: "Operational Data of Burner Control Unit to Room Control Unit",
                         0x12: "0x12 Unknown Command",
+                        0x13: "0x13 Unknown Command",
                         0x16: "Broadcast Service",
-                        0x19: "0x19 Unknown Command",
+                        
                         }
                  }
 
@@ -279,21 +280,29 @@ class EBus(Packet):
     ]
 
 class EBusBulexOpDataRoomControlBurnerControl(Packet):
+    '''
+    Seems to be related to the temperature of the boiler
+
+    state: work in progress
+    '''
     fields_desc = [
-                    XByteField("xx1", 0x00),
-                    XByteField("xx2", 0x00),
+                    XByteField("xx1", 0x00),# always 0x00
+                    XByteField("xx2", 0x00),# always 0x00
                     Data1cField("LT", None), # Lead water target temperature
+                                            # lots of 0 C
+                                            # then from 50 to 77,5 C
                     Data1cField("ST", None), # Service water target temperature
-                    XByteField("xx3", 0xFF),
-                    XByteField("xx4", 0xFF),
-                    XByteField("xx5", 0x00), # ? 
-                    XByteField("xx6", 0xFF),
-                    XByteField("xx7", 0x00),
+                                            # 50 C
+                    XByteField("xx3", 0xFF),# always 0xff
+                    XByteField("xx4", 0xFF),# always 0xff
+                    XByteField("xx5", 0x00),# 0x00 or 0x01
+                    XByteField("xx6", 0xFF),# always 0xff
+                    XByteField("xx7", 0x00),# always 0x00
                     XByteField("CRC", None), # CRC
 
                     XByteField("ACK", 0x00),
-                    ByteField("NN2", 0x01), # Length of data
-                    ByteField("zz", 0x01), # acknowledge? 
+                    ByteField("NN2", 0x01), # Length of data, 1
+                    ByteField("zz", 0x01), # acknowledge? always 1
                     XByteField("CRC", None),
                     XByteField("ACK", 0x00)
     ]
@@ -303,7 +312,6 @@ class EBusBulexOpDataBurnerControltoRoomControl(Packet):
     '''
     state: work in progress
     '''
-# FIXME : some of these packets are with length 8 and fail this structure
     fields_desc = [
                     ByteField("BLK_NUM", 0x01),
                     XByteField("CRC", None), # CRC
@@ -314,9 +322,38 @@ class EBusBulexOpDataBurnerControltoRoomControl(Packet):
 
 class EBusBulexOpDataBurnerControltoRoomControl0(Packet):
     '''
-    state: uknown
+    state: TODO unknown
     '''
     fields_desc = [
+                    XByteField("xx1", None), # many values, probably temperature
+                                             # 0x00 up to 0xff 
+                    XByteField("xx2", None), # 447         xx2       = 0x1
+                                             # 299         xx2       = 0x2
+                                             # 947         xx2       = 0x3
+                                             # 320         xx2       = 0x4
+                    XByteField("xx3", None), #1009         xx3       = 0x10
+                                             #   1         xx3       = 0x4
+                                             # 315         xx3       = 0xc
+                                             # 331         xx3       = 0xd
+                                             #  71         xx3       = 0xe
+                                             # 286         xx3       = 0xf
+                    XByteField("xx4", None), # many values, probably temperature,
+                                             # 0x00 up to 0x50 
+                                             # mainly 0x1d t0 0x2d
+                    XByteField("xx5", None), #  529         xx5       = 0x1f
+                                             #    3         xx5       = 0x2
+                                             #    1         xx5       = 0x37
+                                             # 1309         xx5       = 0x4
+                                             #   39         xx5       = 0x7
+                                             #  132         xx5       = 0x8
+                    XByteField("xx6", None), #  661         xx6       = 0x10
+                                             #    1         xx6       = 0x4
+                                             #   42         xx6       = 0x8
+                                             # 1309         xx6       = 0xf
+                    XByteField("xx7", None), # always 0x00, once 0x0f
+                    XByteField("xx8", None), # 0x00, 0x80, 0x81, 0x82
+                    XByteField("CRC", None),
+                    XByteField("ACK", 0x00)
     ]
 
 
@@ -335,6 +372,61 @@ class EBusBulexOpDataBurnerControltoRoomControl1(Packet):
                     XByteField("xx2", 0xFF),
                     XByteField("CRC", None),
                     XByteField("ACK", 0x00)
+    ]
+
+class EBusBulexOpDataBurnerControltoRoomControl2(Packet):
+    '''
+    state: TODO unknown
+    '''
+    fields_desc = [
+                    XByteField("xx1", None), # 0x02, 0x03
+                    XByteField("xx2", None), # always 0x14
+                    XByteField("xx3", None), # always 0x96
+                    XByteField("xx4", None), # always 0x5a
+                    XByteField("xx5", None), # always 0x5a
+                    XByteField("CRC", None),
+                    XByteField("ACK", 0x00)
+    ]
+
+class EBusBulexSetOpData(Packet):
+    '''
+    state: TODO unknown
+    '''
+    fields_desc = [
+                    ByteEnumField("TB", None, { 0x5c: '0x5c Unknown',
+                                                # below = From the Vaillant reverse engineering
+                                                0x01: 'SetTargetTemperature',
+                                                0x02: 'SetOperationMode',
+                                                0x09: 'SetTimerProgram',
+                                                0x0A: 'SetNightTemperatureHK1',
+                                                0x0B: 'SetHeatingTemperatiureRatio',
+                                                0x11: 'SwitchSWLoadingPunpDelay',
+                                                0x12: 'SetSWLoadingPumpDelay',
+                                                0x13: 'SwitchLegionnairsDiseaseProtection',
+                                                0x14: 'SwitchParallelLoading',
+                                                0x1A: 'SetCylinderMaxTempSolar1',
+                                                0x1B: 'SetCylinderHysteresisSolar1',
+                                                0x1C: 'SetOutsideTempCorr',
+                                                0x1D: 'SetCylinderMaxTempSolar2',
+                                                0x1E: 'SetCylinderHysteresisSolar2',
+                                                0x20: 'ResetSolarYieldKOL1',
+                                                0x21: 'SetSolarFlowRate',
+                                                0x23: 'SwitchEDPumpControl',
+                                                0x27: 'HeatingStatus',
+                                                0x2B: 'SetSystemParameters',
+                                                0x2D: '(unknown)',
+                                                0x3C: 'VR81RemoteControlUnitForVRC'
+                                            })
+    ]
+
+class EBusBulexGetOrSetDeviceConf(Packet):
+    '''
+    state: TODO unknown
+    '''
+    fields_desc = [
+                    ByteEnumField("DB", None, { 0x0e: '0x0e unknown',
+                                                0x29: '0x29 unknown'
+                                            })
     ]
 
 
@@ -375,6 +467,82 @@ class EBusBulexBroadcastDateTime(Packet):
     ]
 
 
+class EBusBulexUnknown03(Packet):
+    '''
+    state: TODO unknown
+
+       1 This packet:  10 08 b5 03 02 00 01 2f 00 0a ff ff ff b5 09 03 29 64 00 8d 00 03 64 00 00 0d 00  
+       1 This packet:  10 08 b5 03 02 00 01 2f 00 0a ff ff ff ff ff  
+     340 This packet:  10 08 b5 03 02 00 01 2f 00 0a ff ff ff ff ff ff ff ff ff ff 0f 00  
+    '''
+    fields_desc = [
+                    XByteField("xx1", None),
+                    XByteField("xx2", None),
+                    XByteField("CRC1", None),
+                    XByteField("ACK", 0x00),
+                    
+    ]
+
+
+class EBusBulexUnknown07(Packet):
+    '''
+    state: TODO unknown
+
+     325 This packet:  10 08 b5 07 01 08 0f 00 02 64 00 41 00  
+     672 This packet:  30 15 b5 07 01 08 0b 00 02 fa 00 0d 00  
+    '''
+    fields_desc = [
+                    XByteField("xx1", None), # 0x08
+                    XByteField("CRC1", None),
+                    XByteField("ACK", 0x00),
+                    ByteField("NN", None), # length ?
+                    XByteField("xx3", None),
+                    XByteField("xx4", None),
+                    XByteField("CRC2", None),
+                    XByteField("ACK", 0x00)
+    ]
+
+class EBusBulexUnknown12(Packet):
+    '''
+    state: TODO unknown
+
+     332 This packet:  03 64 b5 12 02 02 00 66  
+     164 This packet:  03 64 b5 12 02 02 fe 98  
+    '''
+    fields_desc = [
+                    XByteField("xx1", None),# always 0x02
+                    XByteField("xx2", None),# 0x00 or 0xfe
+                    XByteField("CRC", None), # CRC ?
+    ]
+
+
+class EBusIdentification(Packet):
+    '''
+    state: TODO unknown
+
+      68 This packet:  10 08 07 04 00 17 00 0a b5 42 41 49 30 30 05 01 33 02 4e 00  
+     201 This packet:  10 18 07 04 00 ad  
+     201 This packet:  10 38 07 04 00 42  
+     201 This packet:  10 78 07 04 00 07  
+     198 This packet:  10 bb 07 04 00 2a  
+     198 This packet:  10 bc 07 04 00 2b  
+     201 This packet:  10 f8 07 04 00 8d 
+    '''
+    fields_desc = [
+                    XByteField("CRC", None),
+                    XByteField("ACK", 0x00),
+                    ByteField("NN", 0x00), # Length of data
+                    XByteField("HH", 0xb5), # 0xb5 = Vaillant / Bulex
+                    StrFixedLenField("gg", "", 5), # Device ID: "BAI00"
+                    BCDField("vvs1", 0x00), #FIXME , this + next = SW version
+                    BCDField("vvs2", 0x00), 
+                    BCDField("vvh1", 0x00), #FIXME , this + next = HW version
+                    BCDField("vvh2", 0x00), 
+                    XByteField("CRC", None),
+                    XByteField("ACK", 0x00)
+    ]
+
+
 
 # Working layers
 bind_layers(EBus, EBusBulexBroadcast, PB=0xb5, SB=0x16)
@@ -383,10 +551,18 @@ bind_layers(EBusBulexBroadcast, EBusBulexBroadcastOutsideTemperature, TB=0x01)
 
 
 # Work in progress
-#bind_layers(EBus, EBusBulexOpDataRoomControlBurnerControl, PB=0xb5, SB=0x10)
+bind_layers(EBus, EBusIdentification, PB=0x07, SB=0x04)
+bind_layers(EBus, EBusBulexOpDataRoomControlBurnerControl, PB=0xb5, SB=0x10)
 bind_layers(EBus, EBusBulexOpDataBurnerControltoRoomControl, PB=0xb5, SB=0x11)
+bind_layers(EBus, EBusBulexSetOpData, PB=0xb5, SB=0x05)
+bind_layers(EBus, EBusBulexGetOrSetDeviceConf, PB=0xb5, SB=0x09)
+bind_layers(EBus, EBusBulexUnknown03, PB=0xb5, SB=0x03)
+bind_layers(EBus, EBusBulexUnknown07, PB=0xb5, SB=0x07)
+bind_layers(EBus, EBusBulexUnknown12, PB=0xb5, SB=0x12)
 bind_layers(EBusBulexOpDataBurnerControltoRoomControl, EBusBulexOpDataBurnerControltoRoomControl0, BLK_NUM=0)
 bind_layers(EBusBulexOpDataBurnerControltoRoomControl, EBusBulexOpDataBurnerControltoRoomControl1, BLK_NUM=1)
+bind_layers(EBusBulexOpDataBurnerControltoRoomControl, EBusBulexOpDataBurnerControltoRoomControl2, BLK_NUM=2)
+
 
 
 #data = b'\x10\x08\xb5\x11\x01\x00\x88\x00\x08\x5d\x02\x0f\x00\x1f\x10\x00\x80\xd7\x00\xaa\xaa'
@@ -415,18 +591,29 @@ def EBusProcessStream(f):
             if packet_list:
                 # Process packet
                 e = EBus(''.join(packet_list))
-                #if e.haslayer(EBusBulexBroadcast):
-                #if e.haslayer(EBusBulexBroadcastDateTime):
-                #if e.haslayer(EBusBulexBroadcastOutsideTemperature):
-                #if e.haslayer(EBusBulexOpDataBurnerControltoRoomControl):
-                if e.haslayer(EBusBulexOpDataBurnerControltoRoomControl1):
+                if e.haslayer(EBusBulexBroadcastOutsideTemperature):
+                   
+                # if not e.haslayer(EBusBulexBroadcast) and \
+                #    not e.haslayer(EBusBulexBroadcastDateTime) and \
+                #    not e.haslayer(EBusBulexBroadcastOutsideTemperature) and \
+                #    not e.haslayer(EBusBulexOpDataBurnerControltoRoomControl2) and \
+                #    not e.haslayer(EBusBulexOpDataBurnerControltoRoomControl1) and \
+                #    not e.haslayer(EBusBulexOpDataBurnerControltoRoomControl0) and \
+                #    not e.haslayer(EBusBulexSetOpData) and \
+                #    not e.haslayer(EBusBulexGetOrSetDeviceConf) and \
+                #    not e.haslayer(EBusBulexUnknown07) and \
+                #    not e.haslayer(EBusBulexOpDataRoomControlBurnerControl) and \
+                #    not e.haslayer(EBusBulexUnknown12) and \
+                #    not e.haslayer(EBusBulexUnknown03) \
+                #    :
+
                     e.show()
                     # for the fun, print it on the screen
                     print ("")
                     print ("This packet: "), 
                     for i in packet_list:
                         print (binascii.hexlify(i)),
-                    print ("")
+                    print (" ")
                     print ("")
                     print ("")
             packet_list = []
